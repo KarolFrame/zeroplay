7659c1c56cf7c0d6a1bc085328f233ce3f5fb243
Carolina <carolina.peseca@gmail.com>
2025-11-21 12:06:18 +0100

    desable hydration

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -14,7 +14,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch()),
     provideAnimationsAsync(),
   ],
